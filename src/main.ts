c5e9b3ec10f9fca16f653f385595870542a598a1
Gerson M. Veramendi <gerson@veramendi.net>
2023-02-05 10:48:44 -0400

    add swagger api documentation.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import { ConfigService } from "@nestjs/config";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Conexion Lab API')
+    .setDescription('Conexion Lab API documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   app.use(cookieParser());
-  await app.listen(3000);
+
+  const port = configService.get('PORT') ?? 3000;
+  await app.listen(port);
 }
 bootstrap();
